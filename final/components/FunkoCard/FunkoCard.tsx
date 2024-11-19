e4297541c719f2f8a54e772551d0508640c3b146
Chris DeMars <cdemars@digitalocean.com>
2024-11-19 14:03:08 -0500

    adds new edit button with form modal to change data

--- final/components/FunkoCard/FunkoCard.tsx
+++ final/components/FunkoCard/FunkoCard.tsx
@@ -5,9 +5,19 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { FunkoProps } from "../../types/funko";
-import { Button } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
 
 const FunkoCard: React.FC<FunkoProps> = ({ funkos }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       {funkos.map((funko) => {
@@ -34,10 +44,56 @@ const FunkoCard: React.FC<FunkoProps> = ({ funkos }) => {
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small" color="primary">
+              <Button size="small" color="primary" onClick={handleClickOpen}>
                 Edit
               </Button>
             </CardActions>
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Edit Funko</DialogTitle>
+              <DialogContent>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  label="Movie/TV Show"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                  defaultValue={funko.movie_tv_show}
+                />
+                <TextField
+                  margin="dense"
+                  label="Character"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                  defaultValue={funko.character}
+                />
+                <TextField
+                  margin="dense"
+                  label="Year Released"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                  defaultValue={funko.year_release}
+                />
+                <TextField
+                  margin="dense"
+                  label="Number in Line"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                  defaultValue={funko.number}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={handleClose} color="primary">
+                  Save
+                </Button>
+              </DialogActions>
+            </Dialog>
           </Card>
         );
       })}
