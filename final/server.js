8e61aedd3ad6d21c9264f8d3db930439c996e2fe
Chris DeMars <cdemars@digitalocean.com>
2024-12-05 15:28:46 -0600

    hides connection string

--- final/server.js
+++ final/server.js
@@ -9,7 +9,8 @@ const port = process.env.Port || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://doadmin:r7219gPu350peCw4@close-encounters-1a0e761d.mongo.ondigitalocean.com/admin?authSource=admin&tls=true', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const funkoSchema = new mongoose.Schema({
   imageUrl: String,
