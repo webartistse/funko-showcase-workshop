bb03c34c542c6997bb300528a82eb7a82317e116
Chris DeMars <cdemars@digitalocean.com>
2025-01-21 10:23:14 -0600

    deletes code from component

--- start/components/AddFunkoButton/AddFunkoButton.tsx
+++ start/components/AddFunkoButton/AddFunkoButton.tsx
@@ -33,68 +33,9 @@ const AddFunkoButton = ({ setFunkos }: { setFunkos: React.Dispatch<React.SetStat
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '10px' }}>
-      <Button variant="contained" color="secondary" onClick={handleClickOpen}>
-        Add Funko!
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Enter Details</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            name="imageUrl"
-            label="Image URL"
-            type="url"
-            fullWidth
-            value={formValues.imageUrl}
-            onChange={handleInputChange}
-          />
-          <TextField
-            margin="dense"
-            name="source"
-            label="TV/Movie source"
-            type="text"
-            fullWidth
-            value={formValues.source}
-            onChange={handleInputChange}
-          />
-          <TextField
-            margin="dense"
-            name="character"
-            label="Character"
-            type="text"
-            fullWidth
-            value={formValues.character}
-            onChange={handleInputChange}
-          />
-          <TextField
-            margin="dense"
-            name="yearReleased"
-            label="Year Released"
-            type="number"
-            fullWidth
-            value={formValues.yearReleased}
-            onChange={handleInputChange}
-          />
-          <TextField
-            margin="dense"
-            name="numberInLine"
-            label="Number In Line"
-            type="number"
-            fullWidth
-            value={formValues.numberInLine}
-            onChange={handleInputChange}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="secondary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} color="primary">
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
+
+      {/* Code to create Add Button functionality goes here */}
+
     </div>
   );
 };
